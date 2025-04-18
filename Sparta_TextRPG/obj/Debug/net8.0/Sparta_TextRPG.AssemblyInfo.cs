49f17f39e81e2db456d825af578328229022ed94
NileBlue0210 <bjw8594@gmail.com>
2025-04-18 11:57:43 +0900

    to do 수정 리팩토링 사전 준비

--- Sparta_TextRPG/obj/Debug/net8.0/Sparta_TextRPG.AssemblyInfo.cs
+++ Sparta_TextRPG/obj/Debug/net8.0/Sparta_TextRPG.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sparta_TextRPG")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+964bd4904be97e29d2367a194b901da58e46f9b3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sparta_TextRPG")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Sparta_TextRPG")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
